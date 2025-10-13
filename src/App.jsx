806949e874d95084a1b2e452ae7dde9014d63a6d
isinaannn <muhammadsinantm33@gmail.com>
2025-10-14 00:09:11 +0530

    add recommendation card slider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TrendingList from './TrendningList'
+import RecommendationList from './RecommendationList'
 
 
 function App() {
@@ -17,6 +18,18 @@ function App() {
       <div className='link' style={{ display: "flex", justifyContent: "end", paddingTop: "8px", paddingRight: "5px" }}>
         <a href="" style={{ color: "white", fontWeight: "300" }}>View all</a>
       </div>
+
+      <h2 style={{textAlign: "start",fontSize: "25px",marginBottom: "20px"}}>Recommended to you</h2>
+      <div className='recommendation-card-slider-container' style={{overflow: "hidden",width: "100%",overflowX: "scroll",overflowY: "hidden",scrollBehavior: "smooth",scrollbarWidth: "none",}}>
+      <div className='recommendation-card-wrapper' style={{display: "flex",gap: "30px",paddingRight: "30px"}}>
+
+     <RecommendationList />
+     <RecommendationList />
+     <RecommendationList />
+     <RecommendationList />
+   
+     </div>
+     </div>
     </div>
   )
 }
---
+++ src/RecommendationList.jsx
@@ -0,0 +1,28 @@
+import "./recommendationList.css"
+
+export default function RecommendationList(){
+
+
+    return (
+        <div className="recommendation">
+
+            <div className="recommendation-box">
+                <div className="inner-box1 inner-box">
+                    <img src="public/8735bd33-09bd-4407-aa4c-e2f3e0732681.jpeg" alt="" />
+                </div>
+                <div className="inner-box2 inner-box">
+                    <img src="public/9356e3fb-d5d5-4836-8132-bb0644f95a49.jpeg" alt="" />
+                </div>
+                <div className="inner-box3 inner-box">
+                    <img src="public/e73d9161-54dc-4f48-a1b5-7e68ddd9afcb.jpeg" alt="" />
+                </div>
+                <div className="inner-box4 inner-box">
+                    <img src="public/photoshop thumbnail editing poster music video….jpeg" alt="" />
+                </div>
+            </div>
+            <div className="recommendation-details">
+                <p>Ar Rahman .Harris jayaraj .Hiphop Tamizha</p>
+            </div>
+        </div>
+    )
+}
