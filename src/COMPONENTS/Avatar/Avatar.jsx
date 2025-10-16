b0be0a6dbdb6f24c5c995a05123ecf3435c39484
vishnunath c v <vishnunathcv004@gmail.com>
2025-10-16 13:01:17 +0530

    Scale up avatar while translate

--- src/COMPONENTS/Avatar/Avatar.jsx
+++ src/COMPONENTS/Avatar/Avatar.jsx
@@ -7,13 +7,13 @@ const Avatar = ()=>{
         const elem2 = document.querySelector(".avtr");
         const elem3 = document.querySelector(".avtrcontainer");
        if(open.current){
-           console.log(elem.offsetLeft);
-        elem2.style.transform = `translate(${0}px,${0}px)`;
+           console.log(elem.offsetLeft, elem3.offsetWidth);
+        elem2.style.transform = `translate(${0}px,${0}px) scale(${1},${1})`;
         elem.style.transform = `translate(${100}%,${0})`;
         open.current=false;
        }else{
          console.log(elem.offsetLeft);
-        elem2.style.transform = `translate(${elem.offsetLeft+elem3.offsetLeft}px,${elem3.offsetTop}px)`;
+        elem2.style.transform = `translate(${elem.offsetLeft+elem3.offsetLeft+(elem3.offsetWidth/4)}px,${elem3.offsetTop+elem3.offsetWidth/4}px) scale(${2},${2})`;
         elem.style.transform = `translate(${0}px,${0}px)`;
         open.current=true;
        }
