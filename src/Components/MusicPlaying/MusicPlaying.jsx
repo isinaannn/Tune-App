c0bfa7ac2fa1ff3292ce325f96eed4c787e269fc
vishnunath c v <vishnunathcv004@gmail.com>
2025-11-11 15:27:27 +0530

    fix errors in musicPlaying component

--- src/Components/MusicPlaying/MusicPlaying.jsx
+++ src/Components/MusicPlaying/MusicPlaying.jsx
@@ -24,7 +24,7 @@ export default function MusicPlaying() {
                 <div className={styles.musicPlayingBar}>
                     <div className={styles.musicPlayingIcons}>
                         <svg className={`${styles.leftIcon} bi bi-chevron-left`} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
+                            <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
                         </svg>
 
                         <svg className={`${styles.playIcon} bi bi-play-circle`} xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" viewBox="0 0 16 16">
@@ -33,7 +33,7 @@ export default function MusicPlaying() {
                         </svg>
 
                         <svg className={`${styles.rightIcon} bi bi-chevron-right`} xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+                            <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
                         </svg>
 
                     </div>
